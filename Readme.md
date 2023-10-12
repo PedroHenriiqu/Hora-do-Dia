48f0d755a409298bc4f0abc86a7ee63fb4a20a31
Pedro Sousa <pompermayerpedrohenrique@gmail.com>
2023-10-12 20:55:35 -0300

    Nova alterações

--- Readme.md
+++ Readme.md
@@ -1,3 +1,3 @@
-Olá, esse e um projeto simples aonde foi criado um site com interatividade com o sistema que ira mostra a hora no local.
+Olá, esse e um projeto simples, aonde foi criado um site com interatividade com o sistema que ira mostra a hora no local.
 
 Esse projeto foi criado para praticar as linguagens JS, HMTL e CSS.
--- script.js
+++ script.js
@@ -2,25 +2,33 @@
 
 
 function carregar(){
+    var msn = window.document.getElementById('msn')
     var msg = window.document.getElementById('msg')
-    var img = window.document.getElementById('imagem') 
-    var data = new Date()
-    var hora = data.getHours()
+    var img = window.document.getElementById('imagem') // Pega a img
+    var data = new Date() // Parte onde puxa a hora do sistema
+    var hora = data.getHours() //Onde transforma a hora
     msg.innerHTML = `Agora são ${hora} horas.`
+    //Teste logo abaixo
+    //var hora = 7
+
     if (hora >= 0 && hora <= 12 ){
         // Foto manha
         img.src = 'img/manha.png'
-        document.body.style.background = '#f0d0bc'
+        msn.innerHTML = `Bom Dia!!!`
+        document.body.style.background = '#00a2ff'
+
     } else if (hora >= 12 && hora <= 18){
         //Foto tarde
         img.src = 'img/tarde.png'
+        msn.innerHTML = `Boa Tarde!!!`
         document.body.style.background = '#cb5608'
+
     } else {
         //Foto noite
         img.src = 'img/noite.png'
+        msn.innerHTML = `Boa Noite!!!`
         document.body.style.background = '#4e555e'
     }
-    
 
 
 }
